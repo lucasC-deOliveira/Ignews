08a9bf0dbbdea904a83b0aff206cb3953b8cd1c2
LucasC-deOliveira <luckericksen@gmail.com>
2022-03-25 13:02:05 -0300

    title dinamico por pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import styles from "../styles/home.module.scss"
-
+import Head from "next/head"
 
 export default function Home() {
   return (
-   <h1 className={styles.title}>Hello my friendo</h1>
-  ) 
+    <>
+      <Head>
+        <title>Ig News</title>
+      </Head>
+      <h1 className={styles.title}>Hello my friendo
+      </h1>
+    </>
+  )
 }
