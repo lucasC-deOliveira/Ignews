e77d4415921637b12b34454a930cd9b890f4ef2c
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-07-03 09:08:07 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,8 +4,21 @@ import Head from "next/head"
 import { getPrismicClient } from "../../services/prismic"
 import styles from "./styles.module.scss"
 import Prismic from "@prismicio/client"
+import { RichText } from "prismic-dom"
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,41 +26,19 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>
-              03 de junho de 2022
-            </time>
-            <strong>
-              lorem ipsum
-            </strong>
-            <p>
-              fjd fdafdsa fdksafad dakfdkas dfajfdkasf dfajkdsfs fdjsafkdsaf dfjafdjkasfj
-            </p>
-          </a>
-
-          <a>
-            <time>
-              03 de junho de 2022
-            </time>
-            <strong>
-              lorem ipsum
-            </strong>
-            <p>
-              fjd fdafdsa fdksafad dakfdkas dfajfdkasf dfajkdsfs fdjsafkdsaf dfjafdjkasfj
-            </p>
-          </a>
-
-          <a>
-            <time>
-              03 de junho de 2022
-            </time>
-            <strong>
-              lorem ipsum
-            </strong>
-            <p>
-              fjd fdafdsa fdksafad dakfdkas dfajfdkasf dfajkdsfs fdjsafkdsaf dfjafdjkasfj
-            </p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug}>
+              <time>
+                {post.updatedAt}
+              </time>
+              <strong>
+                {post.title}
+              </strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -64,12 +55,23 @@ export const getStaticProps: GetStaticProps = async () => {
     fetch: ['publication.title', 'publication.content'],
     pageSize: 100
   })
-  
-  console.log(JSON.stringify(response,null,2))
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+      })
+    }
+  })
 
   return {
     props: {
-
+      posts
     }
   }
 }
